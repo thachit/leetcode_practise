a302caae64b699132ec5d4187c4c3bc1c4ca66fb
Thach Nguyen <thachnguyen@elsanow.io>
2021-12-20 20:28:31 +0700

    roman to integer

--- src/roman_to_integer.py
+++ src/roman_to_integer.py
@@ -2,31 +2,59 @@
 
 test_cases = [
     {
-        'nums': [2,7,11,15],
-        'target': 9,
-        'result': [0, 1]
+        "input": "III",
+        'result': 3
     },
     {
-        'nums': [3,2,4],
-        'target': 6,
-        'result': [1,2]
+        "input": "LVIII",
+        'result': 58
     },
     {
-        'nums': [3,3],
-        'target': 6,
-        'result': [0, 1]
+        "input": "MCMXCIV",
+        'result': 1994
+    },
+    {
+        "input": "MMXXVI",
+        'result': 2026
     }
 ]
+
+symbol_mapping = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
 
 class Solution:
     def __str__(self):
         return f"Executed {__file__}"
 
+    def romanToInt(self, s: str) -> int:
+        s = s[::-1]
+        total = 0
+        prev = None
+        for i in range(1, len(s) + 1):
+            c = s[i-1:i]
+
+            if prev is None or symbol_mapping[c] >= symbol_mapping[prev]:
+                total = total + symbol_mapping[c]
+            else:
+                total = total - symbol_mapping[c]
+            prev = c
+
+        return total
+
+
 def main():
     s = Solution()
     for test_case in test_cases:
-        result = s.twoSum(test_case['nums'], test_case['target'])
+        result = s.romanToInt(test_case['input'])
         if result == test_case['result']:
-            print('passed')
+            print('passed ', result)
         else:
-            print('failed')
+            print('failed ', result)
